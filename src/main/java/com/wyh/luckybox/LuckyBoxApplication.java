b130f1a875654913ed49d20b61cf1ce7507ae23e
wyh-debug <2435664757@qq.com>
2026-09-16 10:43:48 +0800

    后端

--- src/main/java/com/wyh/luckybox/LuckyBoxApplication.java
+++ src/main/java/com/wyh/luckybox/LuckyBoxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysteryBoxApplication {
+public class LuckyBoxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MysteryBoxApplication.class, args);
+		SpringApplication.run(LuckyBoxApplication.class, args);
 	}
 
 }
